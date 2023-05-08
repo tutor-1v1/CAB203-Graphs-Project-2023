9cf0b62514e3dd648fcbf9ee2fc6aedba733499a
tutor-1v1 <liumeng64@gmail.com>
2023-05-08 14:41:25 +0800

    WeChat cstutorcs CAB203 Graphs Project 2023

---
+++ CAB203 Graphs project.py
@@ -0,0 +1,35 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+import graphs
+import digraphs
+import csv
+
+# Make sure that you implement all of the following functions
+# Run the test suit like:
+# python test_project.py
+# or
+# python3 test_project.py
+# 
+# you can install pytest with
+# pip install pytest 
+# Then you can run
+# pytest test_project.py
+
+def gamesOK(games):
+   # you can use functions from graphs.py like this:
+   x = graphs.arbitrary({1,2,3})  # Delete
+   y = digraphs.arbitrary({1,2,3}) # Delete
+
+def potentialReferees(refereecsvfilename, player1, player2):
+   pass # Delete
+
+def gameReferees(gamePotentialReferees):
+   pass # Delete
+
+def gameSchedule(assignedReferees):
+   pass # Delete
+
+def ranking(games):
+   pass # Delete
